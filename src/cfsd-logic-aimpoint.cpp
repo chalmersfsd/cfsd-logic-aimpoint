04b4a6281847510257089b08df8b9a2797b657b2
lowitz <lovem@student.chalmers.se>
2019-07-02 15:00:31 +0200

    Fixed aimpoint angle offset

--- src/cfsd-logic-aimpoint.cpp
+++ src/cfsd-logic-aimpoint.cpp
@@ -129,7 +129,7 @@ int32_t main(int32_t argc, char **argv) {
             }
           }
 
-          float headingRequest = std::atan2(yAimPoint, xAimPoint) - PI_F / 2.0f; // Offset 90 degrees to the local coordinate system
+          float headingRequest = std::atan2(yAimPoint, xAimPoint);
           headingRequest = headingRequest * lowPassFactor + headingRequestOld * (1.0f - lowPassFactor);
 
 
